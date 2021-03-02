036d34cda60c5ba74577e833936d8913379bd861
BrianReisman <brianreisman@protonmail.com>
2021-03-02 13:16:47 -0500

    connected Nav.js to redux store to render navItems based on logged in status. Added styles. Reordered Nav in App.js

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
 
 import NavStyle from "./NavStyle";
 
+
 const Nav = (props) => {
-  return (
-    <NavStyle>
-      <NavLink to="/sign-up">Sign Up</NavLink>
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/potlucks">Potlucks</NavLink>
-    </NavStyle>
-  );
+  const token = localStorage.getItem("token");
+
+  //extra safe or stupid to to check both localStorage and redux? If there are bugs, try removing one.
+  const NavItems = props.loggedIn && token
+    ? [
+        <NavLink to="/dashboard">Dashboard</NavLink>,
+        <NavLink to="/create-potluck">Create Pockluck</NavLink>,
+        <NavLink to="/">Signout</NavLink>,
+      ]
+    : [
+        <NavLink to="/">Home</NavLink>,
+        <NavLink to="/sign-up">Sign Up</NavLink>,
+        <NavLink to="/login">Login</NavLink>,
+        <NavLink to="/potlucks">Potlucks</NavLink>,
+      ];
+
+  return <NavStyle>{NavItems}</NavStyle>;
 };
 
-export default Nav;
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: state.login.isLoggedIn,
+  };
+};
+
+export default connect(mapStateToProps)(Nav);

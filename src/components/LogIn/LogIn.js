e0905f7b5060ccf3924930d6847b5a13884bd12f
BrianReisman <brianreisman@protonmail.com>
2021-03-04 13:39:29 -0500

    post request working on user sign up after data formatting

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -9,8 +9,8 @@ import { setUserData } from "./../../store/actions/loginActions";
 
 // Initial Sign Up form values
 const initialValues = {
-  email: "testemail@fakeemail.com",
-  password: "abc123",
+  email: "testing2@testing.com",
+  password: "password",
 };
 
 // Yup validation schema
